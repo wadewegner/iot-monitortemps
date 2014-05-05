20402768bce849dae73113ce5e42c50f1515ec76
Wade Wegner <wade.wegner@gmail.com>
2014-05-05 09:05:48 -0600

    Added durability and persistence

--- src/simulator/app.py
+++ src/simulator/app.py
@@ -2,7 +2,7 @@ import pika, os, urlparse, random, pytz
 from time import sleep
 from datetime import datetime
 
-queueName = "message"
+queueName = "task_queue"
 url_str = os.environ.get('CLOUDAMQP_URL', '')
 url = urlparse.urlparse(url_str)
 
@@ -12,13 +12,21 @@ params = pika.ConnectionParameters(
     credentials=pika.PlainCredentials(url.username, url.password))
 
 connection = pika.BlockingConnection(params)
+
+# Open the channel
 channel = connection.channel()
-channel.queue_declare(queue=queueName)
+
+# Declare the queue
+channel.queue_declare(queue=queueName,
+					  durable=True,
+					  exclusive=False,
+					  auto_delete=False)
 
 try:
 
 	while True:
 
+		# Create test data
 		c_r = random.uniform(-1, 1)
 		f_r = random.uniform(-5, 5)
 		c = 25.875 + c_r
@@ -26,11 +34,19 @@ try:
 		now = datetime.now(pytz.utc)
 
 		message = "{0},{1},{2}".format(c, f, now)
-		channel.basic_publish(exchange='', routing_key=queueName, body=message)
+		
+		# Send a message
+		channel.basic_publish(exchange='', 
+							  routing_key=queueName, 
+							  body=message,
+							  properties=pika.BasicProperties(
+		                         delivery_mode = 2, # make message persistent
+		                      ),
+							  mandatory=True)
 
 		print " [x] Sent: %s" % message
 
-		sleep(2)
+		sleep(5)
 
 except KeyboardInterrupt:
 
--- src/worker/app.py
+++ src/worker/app.py
@@ -1,7 +1,8 @@
 import os, psycopg2, urlparse, pika
 
-queueName = "message"
+queueName = "task_queue"
 
+# Define the connection to Postgress
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
@@ -18,18 +19,29 @@ cur = conn.cursor()
 url_str = os.environ.get('CLOUDAMQP_URL', '')
 url = urlparse.urlparse(url_str)
 
+# Define the connection to CloudAMQP
 params = pika.ConnectionParameters(
 	host=url.hostname,
 	virtual_host=url.path[1:],
     credentials=pika.PlainCredentials(url.username, url.password))
 
 connection = pika.BlockingConnection(params)
+
+# Open the channel
 channel = connection.channel()
+
+# Declare the queue
+channel.queue_declare(queue=queueName,
+					  durable=True,
+					  exclusive=False,
+					  auto_delete=False)
 
 def callback(ch, method, properties, body):
 
+	# Split the message
 	records = [x.strip() for x in body.split(',')]
 
+	# Insert into Postgres
 	cur.execute('INSERT INTO iot_monitortemps.readings__c (celsius__c, fahrenheit__c, readingdatetime__c) ' +
 		'VALUES (%s, %s, %s)', 
 		(records[0],records[1],records[2]))
@@ -41,6 +53,8 @@ def callback(ch, method, properties, body):
 	ch.basic_ack(delivery_tag=method.delivery_tag)
 
 try:
+
+	channel.basic_qos(prefetch_count=1)
 
 	channel.basic_consume(callback,
 	    queue=queueName,
